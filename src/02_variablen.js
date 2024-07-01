c6e71a66675b7cf84b3ecbc627da1d3cd6c64750
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-07-01 11:08:14 +0000

    02 / 03 Variablen vs. Konstanten in JS

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -27,24 +27,37 @@
     JS ist ein untypisierte Sprache!
 */
 
-let test; // Deklaration
+// let test; // Deklaration
 
-/* undefined */
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("----------------")
+// /* undefined */
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("----------------")
 
-test = "hi"; // string
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("----------------")
+// test = "hi"; // string
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("----------------")
 
-test = 2; // number
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("----------------")
+// test = 2; // number
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("----------------")
 
-test = true;
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("----------------")
+// test = true;
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("----------------")
+
+/* Variablen vs. Konstanten in JS */
+
+// Variable
+let test; // Variable : Deklaration
+test = "hi"; // Wertzuweisung (Initialisierung)
+test = "hello"; // Überschreiben
+console.log("Inhalt: " + test); // Ausgabe 
+
+// Konstante
+const test2 = "Hi!"; // Deklaration + Wertzuweisung gemeinsam !
+test2 = "hello"; // Fehler !! 
+console.log("Inhalt: " + test2); // Ausgabe 
