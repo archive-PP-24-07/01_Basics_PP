b0e2d5a66fcc8107fcb1965382a8207b4b54264d
Sven Kanter <122672556+cjanta@users.noreply.github.com>
2024-07-01 10:25:32 +0000

    Deklarationen und Wertzuweisung

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -5,4 +5,17 @@
 /**
  * BlockKommentar mit: /**
  */
-console.log("ping");
+// console.log("ping");
+
+// Variablen 01
+// console.log("Hai");
+// console.log(Zung);
+
+let firstName; //Deklaration
+console.log(firstName);
+firstName = "Max"; //Wertzuweisung
+console.log(firstName);
+let familyName = "Mütze"; // Deklarationen und Wertzuweisungen
+console.log(firstName + familyName);
+//Ziel: Hi, ich bin Max Mütze.
+console.log("Hi, ich bin " + firstName + " " + familyName + ".");
