41caca5ade2d91571a8aa58e2ffa3750f37a29df
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-07-01 10:25:18 +0000

    02 / 02 Deklaration + Wertzuweisung I

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -8,3 +8,16 @@
  * 
  */
 
+/***** Variablen 01 *******/
+
+// console.log("Hi");
+// console.log(Hi);
+
+/* Deklaration + Wertzuweisung I */
+
+let firstName; // Deklaration (Definition)
+console.log(firstName); // Ausgabe
+firstName = "Max"; // Wertzuweisung | assignment
+console.log(firstName); // Ausgabe
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // --> Hi, ich bin Max Mütze!
