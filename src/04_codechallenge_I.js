a6c8d58a4e9ee64e590b7dc56120baf9024a641f
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-07-02 10:34:53 +0000

    04 / 01 Iteration 1

---
+++ src/04_codechallenge_I.js
@@ -0,0 +1,34 @@
+
+/*********   CODE CHALLENGE 01 **********/
+ 
+/*
+Geben Sie über eine Prompt-Anforderung eine Zahl ein.
+Geben Sie über eine Prompt-Anforderung eine zweite Zahl ein.
+Berechnen Sie die Summe der beiden Zahlen.
+Geben Sie die Summe der Zahlen in die Konsole aus:
+"Die Summe der Zahlen ist: summe"
+*/
+
+const prompt = require('prompt-sync')({sigint: true});
+
+/*********** Variante 1  ****************/
+// Vorteil: gute Lesbarkeit | aber viele Variablen
+
+let ziffer1, ziffer2, zahl1, zahl2, summe;
+
+ziffer1 = prompt("Bitte Zahl 1 eingeben:");
+ziffer2 = prompt("Bitte Zahl 2 eingeben:");
+
+console.log("Datentyp: " + typeof ziffer1);
+console.log("Datentyp: " + typeof ziffer2);
+
+// Typkonvertierung 1: ziffer --> zahl (mit Nachkomma)
+zahl1 = parseFloat(ziffer1);
+zahl2 = parseFloat(ziffer2);
+
+console.log("Datentyp: " + typeof zahl1);
+console.log("Datentyp: " + typeof zahl2);
+
+summe = zahl1 + zahl2;
+
+console.log("Die Summe der Zahlen ist: " + summe);
