525781b8a9d6b1f4c1a876f7429b778cef2dbbe9
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-07-02 10:55:55 +0000

    04 / 03 Iteration 3

--- src/04_codechallenge_I.js
+++ src/04_codechallenge_I.js
@@ -35,13 +35,25 @@ const prompt = require('prompt-sync')({sigint: true});
 
 /*********** Variante 2  ****************/
 
-// Kompaktere Lösung
+// // Kompaktere Lösung
 
-let zahl1, zahl2, summe;
+// let zahl1, zahl2, summe;
 
-zahl1 = parseInt(prompt("Zahl 1: "));
-zahl2 = parseInt(prompt("Zahl 2: "));
+// zahl1 = parseInt(prompt("Zahl 1: "));
+// zahl2 = parseInt(prompt("Zahl 2: "));
 
-summe = zahl1 + zahl2;
+// summe = zahl1 + zahl2;
 
-console.log("Die Summe der Zahlen ist: " + summe);
+// console.log("Die Summe der Zahlen ist: " + summe);
+
+/*********** Variante 3  ****************/
+// Kompakt, aber schlechter lesbar, unflexibel
+
+console.log
+(
+    "Die Summe ist: " + 
+   (
+    parseFloat(prompt("Bitte Zahl 1 eingeben")) +
+    parseFloat(prompt("Bitte Zahl 2 eingeben"))
+   ) 
+);
