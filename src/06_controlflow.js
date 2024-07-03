f7fc9e85f7d42f55734dd81f24b1b888823f8527
Sven Kanter <122672556+cjanta@users.noreply.github.com>
2024-07-03 09:02:29 +0000

    06_controlflow selektionsübungen

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -1,20 +1,33 @@
 // Entscheidungsstrukturen | control flow
 
 // Deklaration + Assignment
-const ageJohn = 30;
+const ageJohn = 40;
 const ageMark = 30;
 
 // Deklaration
 let isJohnOlder, isJohnEqual;
 
 // Test | Logische Aussage
-isJohnOlder = (ageJohn > ageMark);
-isJohnEqual = (ageJohn == ageMark);
+isJohnOlder = ageJohn > ageMark;
+isJohnEqual = ageJohn == ageMark;
 
 // Ausgabe
-// console.log("ageJohn: " + ageJohn);
-// console.log("ageMark: " + ageMark);
-// console.log("isJohnOlder: " + isJohnOlder);
-// console.log("isJohnEqual: " + isJohnEqual);
-// console.log("------------------");
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
+
+if(isJohnOlder)
+{
+    console.log("John ist älter.");
+}
+else if(isJohnEqual)
+{
+    console.log("John ist gleich alt wie Mark");
+}
+else
+{
+    console.log("John ist jünger.");
+}
 
