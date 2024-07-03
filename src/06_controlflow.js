5754ab842083fcc9e361f8f625478aa8435c10b8
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-07-03 10:28:49 +0000

    06 / 05 SWITCH|CASE 1

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -12,11 +12,11 @@ isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
 // Ausgabe
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
-console.log("------------------");
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
+// console.log("------------------");
 
 /************ IF  ************/
 // TINA --> There is no alternative!
@@ -53,16 +53,49 @@ console.log("------------------");
 /************ IF - ELSE IF  ************/
 // mit alternativen Fällen (älter, jünger, gleich alt)
 
-if (isJohnOlder) 
-{
-    console.log("John ist älter.");  // true
-} 
-else if (isJohnEqual) 
-{
-    console.log("John ist gleich alt.");  // alt. true
-} 
-else 
-{
-    console.log("John ist jünger.");  // false
-}
+// if (isJohnOlder) 
+// {
+//     console.log("John ist älter.");  // true
+// } 
+// else if (isJohnEqual) 
+// {
+//     console.log("John ist gleich alt.");  // alt. true
+// } 
+// else 
+// {
+//     console.log("John ist jünger.");  // false
+// }
 
+
+/****** Fallunterscheidung / SWITCH|CASE 1 ******/
+
+const firstName = "Jane";
+let job;
+
+job = "driver";  // .. fährt TAXI! / UBER
+// job = "diver"; // .. taucht im Rhein! 
+// job = "artist"; // .. malt ein Bild!
+// job = "pilot"; // .. macht etwas anderes! --> default
+// job = "teacher"; // .. unterrichtet!
+// job = "instructor"; // .. unterrichtet!
+
+switch (job) 
+{
+    case "driver": // --> job == "driver"
+        console.log(firstName + " .. fährt TAXI!");
+        break;
+    case "diver": 
+        console.log(firstName + " .. taucht im Rhein!");
+        break;
+    case "artist": 
+        console.log(firstName + "  .. malt ein Bild!");
+        break;
+    case "teacher": 
+    case "instructor": 
+        console.log(firstName + "  .. unterrichtet!");
+        break;
+        
+    default: // Wichtig!
+        console.log(firstName + " .. macht etwas anderes!");
+        break;
+}
