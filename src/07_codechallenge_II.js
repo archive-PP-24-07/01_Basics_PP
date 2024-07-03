b4931be8f30c01029cd56fec66f9ed4954a7e6d2
Sven Kanter <122672556+cjanta@users.noreply.github.com>
2024-07-03 11:24:37 +0000

    02 CC_02 erster Wurf

--- src/07_codechallenge_II.js
+++ src/07_codechallenge_II.js
@@ -16,4 +16,41 @@ Alter 13-17 // Cola
 
 /* Getränke-Challenge */
 
+const prompt = require('prompt-sync')({sigint: true});
+let product;
+let numbers = /^[0-9]+$/;
+let maxTrys = 5;
+let count = 0;
 
+let customerName = prompt("Bitte einen Namen eingeben: ");
+
+let customerAge;
+do
+{
+    count++;
+    customerAge = prompt("Bitte ein Alter eingeben (nur die Zahl): ");
+} 
+while (!customerAge.match(numbers) && count < maxTrys);
+
+if (count >= maxTrys){
+    product = "Milch"
+    console.log("Es wurde keine Zahl eingeben, dann gibt es eben nur Milch.");
+}
+else if(customerAge > 0 && customerAge < 6){
+    product = "Milch"
+}
+else if(customerAge >= 6 && customerAge <= 12){
+    product = "Saft"
+}
+else if(customerAge >= 13 && customerAge <= 17){
+    product = "Cola"
+}
+else if(customerAge >= 18 ){
+    product = "Wein"
+}
+else{
+    product = "Milch"
+    console.log("Es wurde kein sinnvolles Alter angegeben, dann gibt es eben nur Milch.");
+}
+
+console.log("\n" + customerName + " trinkt " + product);
