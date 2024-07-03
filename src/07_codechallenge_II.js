43e67dd5f8e1d2276e5cb4d6b45d163fbf558b27
Sven Kanter <122672556+cjanta@users.noreply.github.com>
2024-07-03 19:47:12 +0000

    07 CC_02 minimalisiert

--- src/07_codechallenge_II.js
+++ src/07_codechallenge_II.js
@@ -15,40 +15,43 @@ Alter 13-17 // Cola
 -->zB: "Max trinkt Cola."
 
 /* Getränke-Challenge */
+console.log("\n" + getCustomerName() + " trinkt " + getProductFromAge(getCustomerAgeValidated()) + ".\n");
 
-const prompt = require('prompt-sync')({sigint: true});
-const products = new Map([
-    ["Milch", "Milch mit 3.5% Fettanteil"],
-    ["Saft", "Saft aus frischen Orangen"],
-    ["Cola", "Cola mit Eis"],
-    ["Wein", "Wein, einen guten Dornfelder aus der Palz"]
-  ]);
-
-const customerName = prompt("Bitte einen Namen eingeben: ");
-const product = getProductFromAge(getCustomerAgeValidated());
-
-console.log("\n" + customerName + " trinkt " + product + ".\n");
+function getCustomerName(){return require('prompt-sync')({sigint: true}).prompt("Bitte einen Namen eingeben: ");}
 
 function getProductFromAge(customerAge){
-    if(customerAge > 0 && customerAge < 6){
-        return products.get("Milch");
+
+    const products = new Map([
+        ["Milch", "Milch mit 3.5% Fettanteil"],
+        ["Saft", "Saft aus frischen Orangen"],
+        ["Cola", "Cola mit Eis"],
+        ["Wein", "Wein, einen guten Dornfelder aus der Palz"]
+        ]);
+
+    function selectProduct(){
+        if(customerAge > 0 && customerAge < 6){
+            return products.get("Milch");
+        }
+        else if(customerAge >= 6 && customerAge <= 12){
+            return products.get("Saft");
+        }
+        else if(customerAge >= 13 && customerAge <= 17){
+            return products.get("Cola");
+        }
+        else if(customerAge >= 18 ){
+            return products.get("Wein");
+        }
+        else{
+            console.log("Es wurde kein sinnvolles Alter angegeben, dann gibt es eben nur: \"" + products.get("Milch") + "\".");
+            return products.get("Milch");
+        }
     }
-    else if(customerAge >= 6 && customerAge <= 12){
-        return products.get("Saft");
-    }
-    else if(customerAge >= 13 && customerAge <= 17){
-        return products.get("Cola");
-    }
-    else if(customerAge >= 18 ){
-        return products.get("Wein");
-    }
-    else{
-        console.log("Es wurde kein sinnvolles Alter angegeben, dann gibt es eben nur: \"" + products.get("Milch") + "\".");
-        return products.get("Milch");
-    }
+
+    return selectProduct();
 }
 
 function getCustomerAgeValidated(){
+
     const numbers = /^[0-9]+$/;
     const maxTrys = 3;   
     let age = 0;
@@ -58,7 +61,7 @@ function getCustomerAgeValidated(){
         do
         {
             count++;
-            age = prompt("Bitte ein Alter eingeben (nur die Zahl): ");
+            age = require('prompt-sync')({sigint: true}).prompt("Bitte ein Alter eingeben (nur die Zahl): ");
         } 
         while (!age.match(numbers) && count < maxTrys);
 
