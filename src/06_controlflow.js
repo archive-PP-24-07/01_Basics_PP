3ab0b2c12dca18d9e4ab82163440e84e37a78371
Sven Kanter <122672556+cjanta@users.noreply.github.com>
2024-07-03 10:10:09 +0000

    06 switch case

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -33,8 +33,8 @@
 
 /****** Fallunterscheidung / SWITCH|CASE 1 ******/
 
-// const firstName = "Jane";
-// let job;
+const firstName = "Jane";
+let job;
 
 // job = "driver";  // .. fährt TAXI! / UBER
 // job = "diver"; // .. taucht im Rhein! 
@@ -42,5 +42,27 @@
 // job = "pilot"; // .. macht etwas anderes! --> default
 // job = "teacher"; // .. unterrichtet!
 // job = "instructor"; // .. unterrichtet!
+
+switch (job) {
+    case "driver":
+        console.log(firstName + " fährt TAXI.");
+    break;
+    case "diver":
+        console.log(firstName + " taucht im Rhein!");
+    break;
+    case "artist":
+        console.log(firstName + " malt ein Bild!");
+    break;
+    case "teacher":
+        console.log(firstName + " unterrichtet!");
+    break;
+    case "instructor":
+        console.log(firstName + " unterrichtet!");
+    break;
+
+    default:
+        console.log(firstName + " macht etwas anderes!");
+    break;
+}
 
 
