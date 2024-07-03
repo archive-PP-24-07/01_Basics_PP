2715b0c6394c5597ed122045beba3f332fd81f11
Sven Kanter <122672556+cjanta@users.noreply.github.com>
2024-07-03 07:33:04 +0000

    05 ini

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -1,17 +1,14 @@
-
-/* Berechnung Alter */
-
-// Deklaration
 let ageJohn, ageMark;
 const birthYearJohn = 1980;
 const birthYearMark = 1990;
 
-// Berechnung Alter
-let year = 2024;
+let year = new Date().getFullYear();
 ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
 
-// Ausgabe
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
 
+let isJohnOlder = ageJohn > ageMark;
+
+console.log("Ist John älter? " + isJohnOlder? "ja" : "nein");
