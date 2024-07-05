5805acee0170c296347df8e1b1f959084ea3d518
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-07-05 09:00:05 +0000

    09 / 03 divide() 3. Iteration

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -12,7 +12,6 @@
 
 const ERROR_STR_DIV = "Teilen durch 0 nicht möglich";
 
-
 // module: division a / b |  test:
 output(divide(4,2));
 output(divide(3,2));
@@ -21,11 +20,13 @@ output(divide(0,2));
 output(divide(3,0));
 output(divide(0,0));
 function divide(a,b) {
-	if (b != 0) {
-		return a / b;
-	} else {
-		return ERROR_STR_DIV;
+	
+	if (b == 0) {
+		return ERROR_STR_DIV;	// Ausnahme
 	}
+
+	return a / b; // Regel
+
 }
 
 
