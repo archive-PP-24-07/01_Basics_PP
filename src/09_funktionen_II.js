ac583a8fafa346ab99d744e69a2353f95c568526
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-07-05 10:26:53 +0000

    09 / 06 startApp() / getNum / getOP

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -4,14 +4,34 @@
 /*
 0. a+b | a-b | a*b | a/b  --> Ergebnis c 
 
-1. Dateneingabe + -überprüfung :  
-2. Auswahl Rechenart : 
+1. Dateneingabe + -überprüfung : semi-check!
+2. Auswahl Rechenart : check!
 3. Fkt. Grundrechenarten : check!
 4. Ausgabe in Konsole : check!
 */
+const prompt = require('prompt-sync')({sigint: true});
 
 const ERROR_STR_DIV = "Teilen durch 0 nicht möglich";
 const ERROR_STR_GEN = "Irgendwas ging schief!";
+
+
+startApp()
+function startApp() {
+	output(calculator(getNum1(),getNum2(),getOp()));
+}
+
+function getNum1() {
+	return parseInt(prompt("Zahl1?: "));
+}
+
+function getNum2() {
+	return parseInt(prompt("Zahl2?: "));
+}
+
+function getOp() {
+	return prompt("OP?: ");
+}
+
 
 /** WAS soll gemacht werden? */
 
