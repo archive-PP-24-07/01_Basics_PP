dc75f290b3391a9d609df490073bd6321310c3b5
Sven Kanter <122672556+cjanta@users.noreply.github.com>
2024-07-08 08:38:11 +0000

    11 THEORIE

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -5,6 +5,19 @@
  *   THEORIE
  * 
  */
+let arr;
+//arr = new Array(); //Konstruktor
+arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+
+function buildSentence(wordArray){
+    const GAP = " ";
+    const PUNCT = ".";
+    let sentence = ""
+    wordArray.forEach(word => {sentence += word + GAP});
+    return sentence.trim() + PUNCT;
+}
+
+out(buildSentence(arr));
 
 
 
@@ -16,19 +29,19 @@
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
-out(getSentence("Ich","bin","Max","Mütze"));
-// out(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
-function getSentence(word1,word2,word3,word4) {
-    const GAP = " ";
-    const PUNCT = ".";
-    let str =   word1 + GAP +
-                word2 + GAP +
-                word3 + GAP +
-                word4 + GAP +
-                PUNCT;
+// out(getSentence("Ich","bin","Max","Mütze"));
+// // out(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+// function getSentence(word1,word2,word3,word4) {
+//     const GAP = " ";
+//     const PUNCT = ".";
+//     let str =   word1 + GAP +
+//                 word2 + GAP +
+//                 word3 + GAP +
+//                 word4 + GAP +
+//                 PUNCT;
 
-    return str;
-}
+//     return str;
+// }
 
 
 
