238e07ba6f95147bd464d6be8408e034d0e624d3
Sven Kanter <122672556+cjanta@users.noreply.github.com>
2024-07-08 09:40:22 +0000

    11 theorie

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -5,20 +5,47 @@
  *   THEORIE
  * 
  */
-let arr;
-//arr = new Array(); //Konstruktor
-arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
 
-function buildSentence(wordArray){
-    const GAP = " ";
-    const PUNCT = ".";
-    let sentence = ""
-    wordArray.forEach(word => {sentence += word + GAP});
-    return sentence.trim() + PUNCT;
+/*** 02. Theorie: Array */
+
+// let arr;
+// arr = new Array(); //Konstruktor
+// arr = []; // Literal
+// arr = [2,11,7,8,10];
+
+// output(arr);
+// output(arr.length); // Anzahl der Elemente
+// output(arr[0]); // Index 0 (1. Pos. im Array)
+// output(arr[arr.length-1]); // letze Pos. / allgemein
+
+/* 02a. Theorie: Schleifen (for-schleife) */
+/* For -Schleifen als allg. Wiederholungs-Struktur */
+
+// Inkrement (untere Grenze --> obere Grenze)
+// for (let i = 0; i < 10; i++) {  // i --> Schleifen-Index
+//     output(i); 
+// }
+
+// Dekrement (obere Grenze --> untere Grenze)
+// for (let i = 10; i > 0; i--) {  // i --> Schleifen-Index
+//     output(i); 
+// }
+
+// Inkrement (var. Schrittweite)
+// for (let i = 10; i <= 100; i += 5) 
+// {
+//    output(i);   
+// }
+
+
+/* 02b. For-Schleife für Array-Index (Iteration)*/
+
+let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+for (let i = 0; i < arr.length; i++) 
+{
+//    output(i); // interner Index 
+   output(arr[i]);  // Mapping  intern --> extern
 }
-
-out(buildSentence(arr));
-
 
 
 /**
@@ -29,27 +56,46 @@ out(buildSentence(arr));
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
-// out(getSentence("Ich","bin","Max","Mütze"));
-// // out(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
-// function getSentence(word1,word2,word3,word4) {
-//     const GAP = " ";
-//     const PUNCT = ".";
-//     let str =   word1 + GAP +
-//                 word2 + GAP +
-//                 word3 + GAP +
-//                 word4 + GAP +
-//                 PUNCT;
+// output(getSentence("Ich","bin","Max","Mütze"));
+// output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+function getSentence(word1,word2,word3,word4,word5,word6) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   word1 + GAP +
+                word2 + GAP +
+                word3 + GAP +
+                word4 + GAP +
+                word5 + GAP +
+                word6 +
+                PUNCT;
 
-//     return str;
-// }
-
-
+    return str;
+}
 
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
-function out(outputStr) {
+function output(outputStr) {
     console.log(outputStr);
 }
 
 
+/*** 01a. Funktionalität mit Array 1 */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+// output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP +
+                arr[5] +
+                PUNCT;
+
+    return str;
+}
